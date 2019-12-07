f6ffb964a7225b2c5da3c7ccdca0e69a24c9c538
Christian Wigert <christian.wigert@gmail.com>
2019-12-07 17:41:03 +0000

    added Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'http://https://chess-game-2019.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
