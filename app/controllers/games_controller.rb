e0c13d57fcc6fd79a5dde1f38186da0145f6d70e
Christian Wigert <christian.wigert@gmail.com>
2019-12-07 19:26:28 +0000

    created game model and new.html.erb view page that has simple form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,17 @@
 class GamesController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def index
+  end
+
+  def new 
+    @game = Game.new
+  end
+
+  def create
+  end
+
+  def show
   end
   
 end
