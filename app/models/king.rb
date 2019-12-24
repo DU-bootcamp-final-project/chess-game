69b333480417df2dc9e933186a4fa84bc50e737e
Christian Wigert <christian.wigert@gmail.com>
2019-12-24 18:33:49 +0000

    created the is_valid? method for King piece

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,3 +1,9 @@
 class King < Piece
 
+  def valid_move?(x_destination, y_destination)
+    x_distance = (cell_x - x_destination).abs <= 1 
+    y_distance = (cell_y - y_destination).abs <= 1
+
+  end
+
 end
