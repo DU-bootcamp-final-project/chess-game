246c3b7060922ff4abfb5f5855f695b9c311013f
Christian Wigert <christian.wigert@gmail.com>
2019-12-27 03:16:21 +0000

    added a few more checks to the king valid move method and began the knight valid move method

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,7 +1,10 @@
 class King < Piece
 
   def valid_move?(x_destination, y_destination)
-    (cell_x - x_destination).abs <= 1 && (cell_y - y_destination).abs <= 1 && is_obstructed?([x_destination, y_destination])
+    return false unless (cell_x - x_destination).abs <= 1 && (cell_y - y_destination).abs <= 1  
+
+    found_piece = Piece.find_by(cell_x: x_destination, cell_y: y_destination, game_id: game_id)
+    (found_piece.nil? || found_piece.color != color) 
   end
 
 end
