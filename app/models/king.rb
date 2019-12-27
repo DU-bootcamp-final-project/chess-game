8ef64fae49f47ec25213e27610cb028675b3f714
Christian Wigert <christian.wigert@gmail.com>
2019-12-27 01:11:48 +0000

    updates to king valid move

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,9 +1,7 @@
 class King < Piece
 
   def valid_move?(x_destination, y_destination)
-    x_distance = (cell_x - x_destination).abs <= 1 
-    y_distance = (cell_y - y_destination).abs <= 1
-
+    (cell_x - x_destination).abs <= 1 && (cell_y - y_destination).abs <= 1 && is_obstructed?([x_destination, y_destination])
   end
 
 end
