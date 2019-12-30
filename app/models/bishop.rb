25f3c2021b50149abb48773f1f0c1940242cc495
Jonathan Garton <jon.garton94@gmail.com>
2019-12-30 20:45:26 +0000

    adding valid moves for bishop and pawn

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,3 +1,8 @@
 class Bishop < Piece
 
+def valid_move?(x_destination, y_destination)
+  [(cell_y - y_destination) / (cell_x - x_destination)].abs == 1
+  && (cell_y - y_destination).abs <= 8 && (cell_x - x_destination).abs <= 8 && is_obstructed([x_destination, y_destination])
+end
+
 end
--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,3 +1,9 @@
 class Pawn < Piece
 
+def valid_move?(x_destination, y_destination, board)
+  if [j] == 2 or [j] == 7 then (cell_x - x_destination).abs == 0 && (cell_y - y_destination).abs == 2
+  else 
+    (cell_x - x_destination).abs == 0 && (cell_y - y_destination).abs == 1 && is_obstructed([x_destination, y_destination])
+end
+
 end
