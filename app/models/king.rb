dfc71b620ee99a6a95323da27a2b46b00acc14d8
Christian Wigert <christian.wigert@gmail.com>
2020-01-03 00:02:06 +0000

    updates to knight and king moves

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,10 +1,10 @@
 class King < Piece
 
   def valid_move?(x_destination, y_destination)
-    return false unless (cell_x - x_destination).abs <= 1 && (cell_y - y_destination).abs <= 1  
+    return false unless is_obstructed?([x_destination, y_destination])
 
-    found_piece = Piece.find_by(cell_x: x_destination, cell_y: y_destination, game_id: game_id)
-    (found_piece.nil? || found_piece.color != color) 
+    (cell_x - x_destination).abs <= 1 && (cell_y - y_destination).abs <= 1  
+
   end
 
 end
