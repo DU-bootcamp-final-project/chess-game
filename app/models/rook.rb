22021ccb171f613aa85d582045e80f1c77d926e1
Melissa Roetker <mlroetker@yahoo.com>
2020-01-03 01:26:10 +0000

    updates to rook.rb

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,8 +1,12 @@
 class Rook < Piece
 
     def valid_move?(x_destination, y_destination)
-        ((cell_x - x_destination).abs >= 1 && cell_x - x_destination).abs <= 8) && (cell_y - y_destination).abs == 0 && is_obstructed?([x_destination, y_destination]) ||
-        ((cell_y - y_destination).abs >= 1 && cell_y - y_destination).abs <= 8) && (cell_x - x_destination).abs == 0 && is_obstructed?([x_destination, y_destination])
+        ((cell_x - x_destination).abs >= 1 && cell_x - x_destination).abs <= 8) && 
+        (cell_y - y_destination).zero? && 
+        is_obstructed?([x_destination, y_destination]) ||
+        ((cell_y - y_destination).abs >= 1 && cell_y - y_destination).abs <= 8) && 
+        (cell_x - x_destination).zero? && 
+        is_obstructed?([x_destination, y_destination])
     end    
 
 end
